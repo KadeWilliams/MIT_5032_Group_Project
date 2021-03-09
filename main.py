3acc0a1675767026c14e452eb12a362173776cd2
kadew <kadewilliams96@gmail.com>
2021-03-09 16:22:05 -0600

    Completed Simple Chart: GBP, USD from January 2020 to February 2021

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd # to read data
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
+
 
 # Display all of the columns in the data set
 pd.set_option('display.max_columns', None)
@@ -8,27 +10,22 @@ pd.set_option('display.max_columns', None)
 # store the csv as a dataframe
 df = pd.read_csv('euro-daily-hist_1999_2020.csv', delimiter=',')
 
-# fill the null (NaN) values with 0 to help with type conversion
-df = df.fillna(0)
-
 '''CLEAN THE COLUMN HEADERS'''
 # Remove the square brackets around
 df.columns = df.columns.str.replace(r'([\[\]])', '')
+
 # Remove the '\' and ':' from 'Period\Unit:' column
 df.columns = df.columns.str.replace(r'[^\w.+\s]', ' ')
 df.columns = df.columns.str.strip()
 
-# # Get the column names for readability and easy transfer
-# text_file = open('columns.txt', 'w')
-# for column in df:
-#     column_names = text_file.write(f'{column}\n')
 
-# print(df['Period\Unit:'])
+# fill the null (NaN) values with 0 to help with type conversion
+df = df.fillna(0)
 
 df_small = df[[
         'Period Unit',
-        'Australian dollar',
-        'Chinese yuan renminbi',
+        # 'Australian dollar',
+        # 'Chinese yuan renminbi',
         # 'Korean won',
         'Brazilian real',
         'UK pound sterling',
@@ -37,46 +34,63 @@ df_small = df[[
 
 headers = {
         'Period Unit': 'Period',
-        'Australian dollar': 'AUD',
-        'Chinese yuan renminbi': 'CNY',
+        # 'Australian dollar': 'AUD',
+        # 'Chinese yuan renminbi': 'CNY',
         # 'Korean won': 'KRW',
-        'Brazilian real': 'BRL',
+        # 'Brazilian real': 'BRL',
         'UK pound sterling': 'GBP',
         'US dollar': 'USD'
 }
 
 
 df_small.rename(columns=headers, inplace=True)
-print(df_small)
+# print(df_small)
 
 
-df_small = df_small[~df_small['AUD'].isin(['-'])]
-df_small = df_small[~df_small['CNY'].isin(['-'])]
+# df_small = df_small[~df_small['AUD'].isin(['-'])]
+# df_small = df_small[~df_small['CNY'].isin(['-'])]
 # df_small = df_small[~df_small['KRW'].isin(['-'])]
-df_small = df_small[~df_small['BRL'].isin(['-'])]
+# df_small = df_small[~df_small['BRL'].isin(['-'])]
 df_small = df_small[~df_small['GBP'].isin(['-'])]
 df_small = df_small[~df_small['USD'].isin(['-'])]
 
 type_conversion_dict = {
         'Period': 'datetime64[ns]',
-        'AUD': float,
-        'CNY': float,
-        'BRL': float,
+        # 'AUD': float,
+        # 'CNY': float,
+        # 'BRL': float,
         'GBP': float,
         'USD': float
 }
 
+df_small['Period'] = pd.to_datetime(df_small['Period'])
+
+df_small = df_small[ df_small['Period'] > '2020-01-01 00:00:00']
+df_small = df_small[ df_small['Period'] < '2021-02-12 00:00:00']
+
+usd_max = df_small['USD'].max()
+usd_min = df_small['USD'].min()
+
+print()
+
+gbp_max = df_small['GBP'].max()
+gbp_min = df_small['GBP'].min()
+
+
 df_small = df_small.astype(type_conversion_dict)
-print(df_small.dtypes)
+df_small.set_index('Period', inplace=True)
+# # Bar Chart of data
+
+ax = df_small.plot()
+plt.scatter('2020-03-19', df_small['GBP'].max(), c='orange')
+plt.scatter('2020-02-17', df_small['GBP'].min(), c='orange')
+plt.scatter('2021-01-06', df_small['USD'].max(), c='purple')
+plt.scatter('2020-03-20', df_small['USD'].min(), c='purple')
+axes = plt.gca()
 
 
-print(df_small.describe())
+ax.legend()
 
-# # Bar Chart of unbound data (from the first entry to the latest entry)
-# ax = df_small.plot.bar(rot=0)
-# plt.show()
-
-# # Write the Data Frame to txt for readability
-df_small.to_csv('small_df.txt', header=True, index=None, sep=' ')
-
+plt.axhline(y=1, color='r', linestyle='--')
+plt.show()
 
