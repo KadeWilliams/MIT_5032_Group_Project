3a134aa02114184b76ab0c4e59e8642b7db6a747
kadew <kadewilliams96@gmail.com>
2021-03-16 11:51:45 -0500

    Graph adjustments

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import pandas as pd # to read data
+import pandas as pd  # to read data
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
-
 
 # Display all of the columns in the data set
 pd.set_option('display.max_columns', None)
@@ -18,30 +17,28 @@ df.columns = df.columns.str.replace(r'([\[\]])', '')
 df.columns = df.columns.str.replace(r'[^\w.+\s]', ' ')
 df.columns = df.columns.str.strip()
 
-
 # fill the null (NaN) values with 0 to help with type conversion
 df = df.fillna(0)
 
 df_small = df[[
-        'Period Unit',
-        # 'Australian dollar',
-        # 'Chinese yuan renminbi',
-        # 'Korean won',
-        'Brazilian real',
-        'UK pound sterling',
-        'US dollar'
+    'Period Unit',
+    # 'Australian dollar',
+    # 'Chinese yuan renminbi',
+    # 'Korean won',
+    'Brazilian real',
+    'UK pound sterling',
+    'US dollar'
 ]]
 
 headers = {
-        'Period Unit': 'Period',
-        # 'Australian dollar': 'AUD',
-        # 'Chinese yuan renminbi': 'CNY',
-        # 'Korean won': 'KRW',
-        # 'Brazilian real': 'BRL',
-        'UK pound sterling': 'GBP',
-        'US dollar': 'USD'
+    'Period Unit': 'Period',
+    # 'Australian dollar': 'AUD',
+    # 'Chinese yuan renminbi': 'CNY',
+    # 'Korean won': 'KRW',
+    # 'Brazilian real': 'BRL',
+    'UK pound sterling': 'GBP',
+    'US dollar': 'USD'
 }
-
 
 df_small.rename(columns=headers, inplace=True)
 # print(df_small)
@@ -55,49 +52,44 @@ df_small = df_small[~df_small['GBP'].isin(['-'])]
 df_small = df_small[~df_small['USD'].isin(['-'])]
 
 type_conversion_dict = {
-        'Period': 'datetime64[ns]',
-        # 'AUD': float,
-        # 'CNY': float,
-        # 'BRL': float,
-        'GBP': float,
-        'USD': float
+    'Period': 'datetime64[ns]',
+    # 'AUD': float,
+    # 'CNY': float,
+    # 'BRL': float,
+    'GBP': float,
+    'USD': float
 }
 
 df_small['Period'] = pd.to_datetime(df_small['Period'])
 
-df_small = df_small[ df_small['Period'] > '2020-01-01 00:00:00']
-df_small = df_small[ df_small['Period'] < '2021-02-12 00:00:00']
+df_small = df_small[df_small['Period'] > '2020-01-01 00:00:00']
+df_small = df_small[df_small['Period'] < '2021-02-12 00:00:00']
 
 usd_max = df_small['USD'].max()
 usd_min = df_small['USD'].min()
 
-
 gbp_max = df_small['GBP'].max()
 gbp_min = df_small['GBP'].min()
-
 
 df_small = df_small.astype(type_conversion_dict)
 df_small.set_index('Period', inplace=True)
 
 # # Bar Chart of data
-ax = df_small.plot()
+ax = df_small.plot(lw=6, zorder=1)
+plt.scatter('2020-03-19', df_small['GBP'].max(), c='black', s=100, zorder=2)
+plt.scatter('2020-02-18', df_small['GBP'].min(), c='black', s=100, zorder=2)
+plt.scatter('2021-01-06', df_small['USD'].max(), c='black', s=100, zorder=2)
+plt.scatter('2020-03-20', df_small['USD'].min(), c='black', s=100, zorder=2)
 
-
-plt.scatter('2020-03-19', df_small['GBP'].max(), c='black')
-plt.scatter('2020-02-18', df_small['GBP'].min(), c='black')
-plt.scatter('2021-01-06', df_small['USD'].max(), c='purple')
-plt.scatter('2020-03-20', df_small['USD'].min(), c='purple')
 plt.xlabel('Period', fontsize=20)
 plt.ylabel('Euro', fontsize=20)
 
-for label in (ax.get_xticklabels()+ax.get_yticklabels()):
-        label.set_fontsize(16)
+for label in (ax.get_xticklabels() + ax.get_yticklabels()):
+    label.set_fontsize(16)
 
 axes = plt.gca()
 
 ax.legend()
 
-plt.axhline(y=1, color='r', linestyle='--')
+plt.axhline(y=1, color='r', linestyle='--', lw=4)
 plt.show()
-
-
